f2bcf0627b5338cc4de7456089ce66a88fae95fb
ksmooi <kaishianmooi@gmail.com>
2024-07-11 12:48:01 +0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -16,4 +16,19 @@ def main():
 @app.route("/echo_user_input", methods=["POST"])
 def echo_input():
     input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text
+    user_agent = request.headers.get('User-Agent')
+    ip_address = request.remote_addr
+    accept_language = request.headers.get('Accept-Language')
+    
+    return f'''
+        <p>You entered: {input_text}</p>
+        <p>Your browser details:</p>
+        <ul>
+            <li>User Agent: {user_agent}</li>
+            <li>IP Address: {ip_address}</li>
+            <li>Accept Language: {accept_language}</li>
+        </ul>
+        '''
+
+if __name__ == "__main__":
+    app.run(debug=True)
